d7f78d1bb16c191e7ae655812087bc3b3de58839
Ken Ding <aenkil@gmail.com>
2018-01-03 00:05:21 +1100

    Modify NettyAppServer to fetchData and send it back JSON

--- src/main/scala/com/choonkending/pasxolio/NettyAppServer.scala
+++ src/main/scala/com/choonkending/pasxolio/NettyAppServer.scala
@@ -3,14 +3,21 @@ package com.choonkending.pasxolio
 import unfiltered.netty.Server
 import unfiltered.netty.cycle.Planify
 import unfiltered.request._
-import unfiltered.response.ResponseString
+import unfiltered.response._
 
 class NettyAppServer {
   private val port = 3000
 
   val server: Server = unfiltered.netty.Server.http(port)
     .plan(Planify {
-        case req@GET(Path("/stocks")) => ResponseString("Stocks requested")
+        case req@GET(Path("/stocks")) => {
+          val stocks = FetchStocksController.fetchStocks()
+          stocks match {
+            case Right(s) => ResponseString(StocksEncoder.toJson(s).spaces2)
+            case _ => InternalServerError ~> ResponseString("Something went wrong")
+          }
+
+        }
       }
     )
 
