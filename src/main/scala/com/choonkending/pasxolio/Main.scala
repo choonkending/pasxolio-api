69fa113804dd5b1455f2072bc1145e44a9ddac25
Ken Ding <aenkil@gmail.com>
2018-01-03 00:05:37 +1100

    Modify Main to invoke NettyAppServer

--- src/main/scala/com/choonkending/pasxolio/Main.scala
+++ src/main/scala/com/choonkending/pasxolio/Main.scala
@@ -1,11 +1,9 @@
 package com.choonkending.pasxolio
 
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val stocks = FetchStocksController.fetchStocks()
-    stocks match {
-      case Right(s) => println(s)
-      case Left(e) => println(e)
-    }
+    val server = new NettyAppServer()
+    server.start()
   }
 }
